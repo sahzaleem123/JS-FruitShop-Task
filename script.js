dede7a9be421037a5bfc34078cf02d95f34c06d8
SYED ALEEM HUSSAIN <49521238+sahzaleem123@users.noreply.github.com>
2019-06-19 23:59:40 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,101 @@
-let total = 0;
-let applequantity=0;
-let orangequanity=0;
-let bananaquanity=0;
+var haveMoney= 0;
+var totalBill= 0;
+var purchase= 0;
+var appleQuantity= 0;
+var orangeQuanity= 0;
+var bananaQuanity= 0;
 
-let qtyValue = document.querySelector('.quantity');
-let totalBill = document.querySelector('.totalbill');
 
-function addapple(){
-    applequantity++;
-    qtyValue.value =applequantity;
-    total=applequantity*10+orangequanity*15+bananaquanity*20;
-    totalBill.innerHTML=total;  
+ 
+function haveCash() {
+  haveMoney = document.querySelector('.have-cash').value;
 }
-function minusapple(){
-    applequantity--;
-    qtyValue.value =applequantity;
-    total=applequantity*10+orangequanity*15+bananaquanity*20;
-    totalBill.innerHTML=total;
+
+function fullBill() {
+   document.querySelector("span").innerHTML = totalBill;
+}
+
+function addApple(){
+    if (haveMoney >= 10) {
+        appleQuantity += 1;
+        purchase += 10;
+        haveMoney = haveMoney - purchase;
+        totalBill = purchase + totalBill;
+        purchase = 0;
+        document.querySelector(".app-quantity").value = appleQuantity;
+        fullBill();
+
+    }
+    else {
+        alert("Not Enough Money to Buy Apple")
+    }
+}
+
+function minusApple(){
+    if (totalBill > 0 && appleQuantity > 0) {
+        appleQuantity -= 1;
+        purchase += 10;
+        haveMoney = haveMoney + purchase;
+        totalBill = totalBill - purchase;
+        purchase = 0;
+        document.querySelector(".app-quantity").value = appleQuantity;
+        fullBill();
+    }
+}
+function addOrange() {
+
+    if (haveMoney >= 15) {
+        orangeQuanity += 1;
+        purchase += 15;
+        haveMoney = haveMoney - purchase;
+        totalBill = purchase + totalBill;
+        purchase = 0;
+        document.querySelector(".org-quantity").value = orangeQuanity;
+        fullBill();
+
+    }
+    else {
+        alert("Not Enough Money to Buy Orange")
+    }
+}
+function minusOrange() {
+
+    if (totalBill > 0 && orangeQuanity > 0) {
+        orangeQuanity -= 1;
+        purchase += 15;
+        haveMoney = haveMoney + purchase;
+        totalBill = totalBill - purchase;
+        purchase = 0;
+        document.querySelector(".org-quantity").value = orangeQuanity;
+        fullBill();
+    }
+
+}
+function addBanana() {
+
+    if (haveMoney >= 7) {
+        bananaQuanity += 1;
+        purchase += 7;
+        haveMoney = haveMoney - purchase;
+        totalBill = purchase + totalBill;
+        purchase = 0;
+        document.querySelector(".ban-quantity").value = bananaQuanity;
+        fullBill();
+
+    }
+    else {
+        alert("Not Enough Money to Buy Banana")
+    }
+}
+function minusBanana() {
+
+    if (totalBill > 0 && bananaQuanity > 0) {
+        bananaQuanity -= 1;
+        purchase += 7;
+        haveMoney = haveMoney + purchase;
+        totalBill = totalBill - purchase;
+        purchase = 0;
+        document.querySelector(".ban-quantity").value = bananaQuanity;
+        fullBill();
+    }
 }
